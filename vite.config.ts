d25d6ee940632494c2630ba217ac3bf43a36b8b3
Clare-Ai-Aimer <hui.yu@clare.ai>
2025-05-26 14:53:24 +0800

    update consume

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import UnoCSS from "unocss/vite";
-import { presetUno, presetAttributify, presetTypography } from "unocss";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import UnoCSS from 'unocss/vite'
+import { presetUno, presetAttributify, presetTypography } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yuhui-resume/',
   plugins: [
     react(),
     UnoCSS({
       presets: [presetUno(), presetAttributify(), presetTypography()],
     }),
   ],
-});
+})
